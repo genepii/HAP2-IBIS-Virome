318fc3d71c9ec0cada73100b9e18a9f70969adcc
Hussein ANANI01 <ananihu@chu-lyon.fr@di3969su.chu-lyon.fr>
2024-03-05 15:26:57 +0100

    message de modification

--- FIGURE4.R
+++ FIGURE4.R
@@ -31,7 +31,7 @@ boot.ci(Mboot,
 
 data <- read.delim("WBC_dynamics.txt", stringsAsFactors = FALSE)
 
-custom_colors <- c("HAP" = "red", "NO_HAP" = "blue")
+custom_colors <- c("Upcoming HAP" = "pink", "NO_HAP" = "blue")
 
 WBC_dynamics <- ggplot(data, aes(x = DAY, y = WBC, group = GROUP, color = GROUP)) +
   geom_line() +
@@ -65,7 +65,7 @@ dev.off()
 
 data <- read.delim("Hellinger_dynamics.txt", stringsAsFactors = FALSE)
 
-custom_colors <- c("HAP" = "red", "NO_HAP" = "blue")
+custom_colors <- c("Upcoming HAP" = "pink", "NO_HAP" = "blue")
 
 Hellinger_dynamics <- ggplot(data, aes(x = DAY, y = Hellinger, group = GROUP, color = GROUP)) +
   geom_line() +
@@ -99,7 +99,7 @@ dev.off()
 
 data <- read.delim("Sorensen_dynamics.txt", stringsAsFactors = FALSE)
 
-custom_colors <- c("HAP" = "red", "NO_HAP" = "blue")
+custom_colors <- c("Upcoming HAP" = "pink", "NO_HAP" = "blue")
 
 Sorensen_dynamics <- ggplot(data, aes(x = DAY, y = Sorensen, group = GROUP, color = GROUP)) +
   geom_line() +
@@ -132,7 +132,7 @@ dev.off()
 
 data <- read.delim("Caudoviricetes_dynamics.txt", stringsAsFactors = FALSE)
 
-custom_colors <- c("HAP" = "red", "NO_HAP" = "blue")
+custom_colors <- c("Upcoming HAP" = "pink", "NO_HAP" = "blue")
 
 Caudoviricetes_dynamics <- ggplot(data, aes(x = DAY, y = Caudoviricetes, group = GROUP, color = GROUP)) +
   geom_line() +
@@ -161,7 +161,51 @@ pdf("Caudoviricetes_dynamics.pdf",width=15,height=10);
 Caudoviricetes_dynamics_stat
 dev.off()
 
-# FIGURE 4C - Lefse HAP contigs
+# FIGURE 4C - Fisher HAP/no HAP contigs
+
+#Use Fisher test to identify discriminant contigs in HAP and no HAP signature 6 days before the HAP onset.
+#Input table : Presence absence counts
+
+otu_data <- read.delim("RPKM_NEW_WTA_deconta_B_50_01.txt", header = TRUE)
+
+for (n in 1:nrow(otu_data)){
+  otu_data_2<-otu_data[n,-1]
+  otu_data_3<-rbind(otu_data_2,apply(otu_data[-n,-1],2,FUN=sum))
+  otu_data$p.value[n]<-fisher.test(otu_data_3)$p.value
+}
+
+
+library(ggplot2)
+
+data <- read.delim("HAP_noHAP_fisher_output.txt")
+
+HAP_noHAP_specific_contigs <- ggplot(data, aes(reorder(OTU, PVAL), PVAL, fill = CLASS)) +
+  geom_bar(stat = "identity", width = 0.8, size = 0.3) +
+  coord_flip() +
+  theme_bw() +
+  scale_fill_manual(values = c("Caudoviricetes" = "pink2","Other bacteriophages" ="#ffcc00", "Unclassified viruses" = "grey", "Eukaryotic viruses" = "red")) +
+  theme(strip.placement = "outside",
+        strip.text.y = element_text(angle = 0)) +
+  labs(title = "", x = "Signicant contigs", y = "Fisher test -log10(adj. P Value)")+
+  theme_classic() +
+  theme(panel.background = element_rect(fill = "white"),
+        strip.background = element_blank(),
+        strip.text = element_text(size=30),
+        legend.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_text(size = 30),
+        axis.title.x = element_text(size = 30),
+        axis.text.x = element_text(size = 30),
+        plot.title = element_blank(),
+        legend.text = element_text(size = 30),
+        legend.position = "bottom") + facet_wrap(~GROUP)
+
+
+pdf("HAP_noHAP_signature_contigs_fisher.pdf",width=16,height=11);
+HAP_noHAP_specific_contigs
+dev.off()
+
+# FIGURE 4D - Lefse HAP contigs
 
 #Use LEfSe to identify discriminant contigs in the HAP signature 6 days before the HAP onset.
 #Input table : ALR/Presence absence counts
@@ -175,26 +219,88 @@ library(ggplot2)
 
 data <- read.delim("HAP_lefse_output.res")
 
-HAP_signature_contigs <- ggplot(data, aes(reorder(Taxon, LDA), LDA, fill = Group)) +
+HAP_discriminant_contigs <- ggplot(data, aes(reorder(Taxon, LDA), LDA, fill = CLASS)) +
   geom_bar(stat = "identity", width = 0.7, size = 0.5) +
   coord_flip() +
   theme_bw() +
-  scale_fill_manual(values = c("Contigs associated with 5-3 period days before HAP onset" = "#cc00cc","Contigs associated with 3-0 period days before HAP onset" ="#ffcc00")) +
+  scale_fill_manual(values = c("Caudoviricetes" = "pink2","Other bacteriophages" ="#ffcc00", "Unclassified viruses" = "grey") +
   theme(strip.placement = "outside",
         strip.text.y = element_text(angle = 0)) +
-  labs(title = "LEfSe of contigs in HAP signature", x = "HAP patients discriminant viral contigs", y = "LDA score (Log10)")+
+  labs(title = "LEfSe of contigs in HAP signature", x = "Discriminant contigs", y = "LDA score (Log10)")+
   theme_classic() +
   theme(panel.background = element_rect(fill = "white"),
+        strip.background = element_blank(),
+        strip.text = element_text(size=30),
         legend.title = element_blank(),
         axis.text.y = element_blank(),
         axis.title.y = element_text(size = 30),
-        axis.title.x = element_text(size = 20),
-        axis.text.x = element_text(size = 20),
+        axis.title.x = element_text(size = 30),
+        axis.text.x = element_text(size = 30),
         plot.title = element_blank(),
-        legend.text = element_text(size = 20),
-        legend.position = "bottom") 
+        legend.text = element_text(size = 30),
+        legend.position = "bottom") + facet_wrap(~Group)
 
 
-pdf("HAP_signature_contigs.pdf",width=16,height=11);
-HAP_signature_contigs
+pdf("HAP_lefse_output.pdf",width=16,height=11);
+HAP_discriminant_contigs
+dev.off()
+
+# FIGURE 4E Chordiagram HAP
+
+#Run MAASLIN2 on RPKM counts of HAP-associated contigs with relative abundance of the core respiratory bacteriome
+
+library(Maaslin2)
+
+df_input_data = read.table(file = "RPKMcounts_HAP_associated.txt",
+                          header = TRUE,
+                          sep              = "\t", 
+                          row.names        = 1,
+                          stringsAsFactors = FALSE)
+
+df_input_metadata = read.table(file = "16S_Core_respiratory_bacteriome.txt", 
+                              header           = TRUE, 
+                              sep              = "\t", 
+                              row.names        = 1,
+                              stringsAsFactors = FALSE)
+
+
+fitData.ctrls.lefse= Maaslin2(
+  input_data = df_input_data, 
+  input_metadata = df_input_metadata,
+  output = "Correlations_between_viral_contigs_and_core_bacteriome",
+  normalization = "none",
+  transform = "none",
+  random_effects = c("Samples"))
+
+
+#Use Maaslin2 output in Chordiagram
+
+data <- read.delim("Correlations_viral_Core_Bacteriome_HAP.txt", header = TRUE, stringsAsFactors = FALSE)
+
+library(ggplot2)
+library(circlize)
+library(scales)
+library(reshape)
+
+grid.col = c(Fusobacterium = "red", Haemophilus = "orange", Prevotella = "green", Streptococcus = "blue", Veillonella = "purple")
+col_fun = colorRamp2(c(-100,-50, -1 ,0, 1, 50), c("red", "pink", "white", "lightblue","blue"))
+
+pdf("chordDiagram_HAP.pdf",width=20,height=20)
+chordDiagram(data, big.gap = 10, symmetric = TRUE, annotationTrack = c("grid","name"), col = col_fun, grid.col = grid.col, scale = FALSE)
+dev.off()
+
+# FIGURE 4F Chordiagram NO HAP
+
+data <- read.delim("Correlations_viral_Core_Bacteriome_NOHAP.txt", header = TRUE, stringsAsFactors = FALSE)
+
+library(ggplot2)
+library(circlize)
+library(scales)
+library(reshape)
+
+grid.col = c(Fusobacterium = "red", Haemophilus = "orange", Prevotella = "green", Streptococcus = "blue", Veillonella = "purple")
+col_fun = colorRamp2(c(0, 1, 50,100), c("white", "lightblue","blue"))
+
+pdf("chordDiagram_noHAP.pdf",width=20,height=20)
+chordDiagram(data, big.gap = 10, symmetric = TRUE, annotationTrack = c("grid","name"), col = col_fun, grid.col = grid.col, scale = FALSE)
 dev.off()
