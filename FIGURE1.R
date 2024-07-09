f3368f397dbef4fd35dfe1eab68a7aecb226645a
Hussein ANANI01 <ananihu@chu-lyon.fr@di3969su.chu-lyon.fr>
2024-07-09 14:35:06 +0200

    message de modification

--- FIGURE1.R
+++ FIGURE1.R
@@ -2,33 +2,54 @@
 #### CODE USED TO GENERATE FIGURE 1
 ############################################################################################################################
 
-# ALR heatmap FIGURE 1A
+# heatmap FIGURE 1A
 
-library(pheatmap)
-matrix <- read.delim("counts.txt", header = TRUE, sep = "\t", row.names = 1)
-matrix <- na.omit(matrix)
-transformed_matrix <- log(matrix+0.01)
-my_sample_col <- read.table("metadata.txt", header = TRUE, sep = "\t",row.names = 1)
+library(gplots)
+library(ComplexHeatmap)
 
-family_sums <- rowSums(transformed_matrix)
+# Read the matrix and keep NA values
+matrix <- read.delim("RPKMcounts.txt", header = TRUE, sep = "\t", row.names = 1)
+
+# Perform log transformation, handling zero values to avoid -Inf
+transformed_matrix <- log10(matrix)
+transformed_matrix[transformed_matrix=="-Inf"] <- NA
+
+# Read the sample annotations
+my_sample_col <- read.table("metadata.txt", header = TRUE, sep = "\t", row.names = 1)
+
+# Select top 10 families based on row sums
+family_sums <- rowSums(transformed_matrix, na.rm = TRUE)
 top10_families <- names(sort(family_sums, decreasing = TRUE))[1:10]
 matrix_top10 <- transformed_matrix[top10_families, ]
 
 
-ALR_heatmap <- pheatmap(
-  matrix_top10,
-  color=colorRampPalette(c("navy", "white", "firebrick3"))(50),
-  annotation_col = my_sample_col,
-  cluster_cols = TRUE,
-  cluster_rows = TRUE,
-  fontsize = 20,
-  fontsize_row = 30,
-  legend = TRUE,
-  display_numbers = F,
-  annotation_legend = TRUE, show_colnames = FALSE, show_rownames = T
+col = list(HAP.condition = c("HAP" = "red", "no HAP" = "blue"),
+           ARDS.condition = c("ARDS" = "green", "no ARDS" = "purple"),
+           Death = c("Deceased" = "yellow", "Alive" = "orange"))
+# Create the heatmap annotation
+ha <- HeatmapAnnotation(
+  HAP.condition = my_sample_col$HAP.condition, 
+  ARDS.condition = my_sample_col$ARDS.condition, 
+  Death = my_sample_col$Death, 
+  HAP.onset = my_sample_col$HAP.onset,
+  col = col,
+  annotation_legend_param = list(
+    HAP.condition = list(title_gp = gpar(fontsize = 30), labels_gp = gpar(fontsize = 30)),
+    ARDS.condition = list(title_gp = gpar(fontsize = 30), labels_gp = gpar(fontsize = 30)),
+    Death = list(title_gp = gpar(fontsize = 30), labels_gp = gpar(fontsize = 30))
+  ),
+  annotation_name_gp = gpar(fontsize = 30)
 )
-pdf("ALR_heatmap.pdf",width=20,height=14);
-ALR_heatmap
+
+
+pdf("heatmap.pdf", width = 20, height = 14)
+
+# Combine the heatmap and the annotation
+Heatmap(matrix_top10, name = "log10RPKM",
+        top_annotation = ha,
+        row_names_gp = gpar(fontsize = 30),
+        column_names_gp = gpar(fontsize = 0))
+
 dev.off()
 
 # FIGURE 1B - Phages lifestyles
@@ -40,7 +61,7 @@ VIBRANT_run.py -i vOTUs.fasta -t 114 -folder VIBRANT_results -virome
 
 library(ggplot2)
 
-# Define the data
+# Define the data (Vibrant output)
 data <- data.frame(
   Sample = c("HAP", "NO HAP", "HAP", "NO HAP"),
   Virus = c("Lytic", "Lytic", "Lysogenic", "Lysogenic"),
@@ -83,7 +104,7 @@ dev.off()
 # BRAY CURTIS
 
 library(vegan)
-data1<-read.delim("counts.txt", row.names = 1)
+data1<-read.delim("RPKMcounts.txt", row.names = 1)
 dataTransposed1<-t(data1)
 dis <- vegdist(dataTransposed1, method = "bray")
 dis2<-as.matrix(dis)
@@ -92,7 +113,7 @@ write.table(dis2,"WeightedBrayCurtis.txt", sep = '\t')
 # Hellinger
 
 library(adespatial)
-data2 <-read.delim("ALRcounts.txt", row.names=1)
+data2 <-read.delim("log10(RPKM+1)counts.txt", row.names=1)
 dataTransposed2 <-t(data2)
 dist.hel <-dist.ldc(dataTransposed2, method = "hellinger")
 dist2 <-as.matrix(dist.hel)
@@ -160,16 +181,16 @@ stat.test <- stat.test %>% add_xy_position(x = "GROUP")
 WBC_Between_within_violin <- ggplot(data, aes(GROUP, WBC)) +
   geom_violin(aes(fill = GROUP), color = "black", trim = TRUE) +  
   geom_boxplot(aes(fill = GROUP), width = 0.05, color = "black", outlier.shape = NA) +  
-  scale_fill_manual(values = c("Between-patients" = "magenta", "Within-patients" = "green3"), name = "GROUP") +  
+  scale_fill_manual(values = c("Inter patients" = "magenta", "Intra patients" = "green3"), name = "GROUP") +  
   stat_pvalue_manual(stat.test, tip.length = 0, size = 20, y.position = 1.1, bracket.size = 2) +
   labs(subtitle = get_test_label(stat.test, detailed = TRUE)) +
   theme_classic() +
   theme(axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 30),
-        axis.text.x = element_text(size = 30, colour = "black", face = "bold"),
-        axis.text.y = element_text(size = 30),
+        axis.title.y = element_text(size = 40),
+        axis.text.x = element_text(size = 40, colour = "black", face = "bold"),
+        axis.text.y = element_text(size = 40),
         plot.subtitle = element_text(size = 30),
-        legend.text = element_text(size = 30),
+        legend.text = element_text(size = 40),
         legend.position = "none") +
   labs(
     subtitle = get_test_label(stat.test, detailed = TRUE),
@@ -206,16 +227,16 @@ stat.test <- stat.test %>% add_xy_position(x = "GROUP")
 Hellinger_Between_within_violin_HAP <- ggplot(data, aes(GROUP, Hellinger)) +
   geom_violin(aes(fill = GROUP), color = "black", trim = TRUE) +  
   geom_boxplot(aes(fill = GROUP), width = 0.05, color = "black", outlier.shape = NA) +  
-  scale_fill_manual(values = c("Between-patients" = "magenta", "Within-patients" = "green3"), name = "GROUP") +  
+  scale_fill_manual(values = c("Inter patients" = "magenta", "Intra patients" = "green3"), name = "GROUP") +  
   stat_pvalue_manual(stat.test, tip.length = 0, size = 20, y.position = 1.5, bracket.size = 2) +
   labs(subtitle = get_test_label(stat.test, detailed = TRUE)) +
   theme_classic() +
   theme(axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 30),
-        axis.text.x = element_text(size = 30, colour = "black", face = "bold"),
-        axis.text.y = element_text(size = 30),
+        axis.title.y = element_text(size = 40),
+        axis.text.x = element_text(size = 40, colour = "black", face = "bold"),
+        axis.text.y = element_text(size = 40),
         plot.subtitle = element_text(size = 30),
-        legend.text = element_text(size = 30),
+        legend.text = element_text(size = 40),
         legend.position = "none") +
   labs(
     subtitle = get_test_label(stat.test, detailed = TRUE),
@@ -253,16 +274,16 @@ stat.test <- stat.test %>% add_xy_position(x = "GROUP")
 Sorensen_Between_within_violin_HAP <- ggplot(data, aes(GROUP, Sorensen)) +
   geom_violin(aes(fill = GROUP), color = "black", trim = TRUE) +  
   geom_boxplot(aes(fill = GROUP), width = 0.05, color = "black", outlier.shape = NA) +  
-  scale_fill_manual(values = c("Between-patients" = "magenta", "Within-patients" = "green3"), name = "GROUP") +  
+  scale_fill_manual(values = c("Inter patients" = "magenta", "Intra patients" = "green3"), name = "GROUP") + 
   stat_pvalue_manual(stat.test, tip.length = 0, size = 20, y.position = 1.1, bracket.size = 2) +
   labs(subtitle = get_test_label(stat.test, detailed = TRUE)) +
   theme_classic() +
   theme(axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 30),
-        axis.text.x = element_text(size = 30, colour = "black", face = "bold"),
-        axis.text.y = element_text(size = 30),
+        axis.title.y = element_text(size = 40),
+        axis.text.x = element_text(size = 40, colour = "black", face = "bold"),
+        axis.text.y = element_text(size = 40),
         plot.subtitle = element_text(size = 30),
-        legend.text = element_text(size = 30),
+        legend.text = element_text(size = 40),
         legend.position = "none") +
   labs(
     subtitle = get_test_label(stat.test, detailed = TRUE),
